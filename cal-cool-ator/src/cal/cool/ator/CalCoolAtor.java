2f94b6f845c2f46fdba052d7456c5e4287db8276
Marek <Marek@192.168.100.19>
2017-04-14 21:47:26 +0200

    testy:add,sub,factorial,neg,div,mul

---
+++ cal-cool-ator/src/cal/cool/ator/CalCoolAtor.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal.cool.ator;
+
+/**
+ *
+ * @author Marek
+ */
+public class CalCoolAtor {
+
+    public double add(double x1,double x2)
+    {
+        return x1+x2;
+    }
+ 
+    /**
+     *
+     * @param x1
+     * @return
+     */
+    public long factorial(long x1) 
+   {
+       return 0;
+   }
+   public double sub(double x1,double x2)
+   {
+       return x1-x2;
+   }
+   public double multiply(double x1,double x2)
+   {
+       return 0;
+   }
+   public double divide(double x1,double x2)
+   {
+       return 0;
+   }
+   public double sqrt(double x1)
+   {
+       return 0;
+   }
+   public double power(double x1,double y1)
+   {
+       return 0;
+   }
+   public double neg(double x1)
+   {
+       return 0;
+   }
+    
+}
+    
+
